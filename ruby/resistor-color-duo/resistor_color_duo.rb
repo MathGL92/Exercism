93949b15434da01f13e4e3e8bf2598e6e76170ee
Mathieu Longe <mathieu.longe@orange.fr>
2020-10-31 09:07:13 +1100

    all tests passing

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -6,3 +6,14 @@ To get started with TDD, see the `README.md` file in your
 `ruby/resistor-color-duo` directory.
 =end
 
+class ResistorColorDuo
+  COLORS = %w(black brown red orange yellow green blue violet grey white)
+
+  def self.value(array)
+    # input array of color names
+    # only keep 2 first elements of array
+    # iterate through array and replace each element by the index of this element in COLORS, join them , transform to integer
+    array.first(2).map.with_index { |color, index| COLORS.index(color) }.join('').to_i
+    # output 2 digit integer (even if more than 2 colors)
+  end
+end
