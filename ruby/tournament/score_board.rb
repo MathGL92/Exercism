9712272fbad5d7cac32424d6e41e7fd7af6c24f0
Mathieu Longe <mathieu.longe@orange.fr>
2020-11-10 19:37:39 +1100

    refactoring score board

--- ruby/tournament/score_board.rb
+++ ruby/tournament/score_board.rb
@@ -1,17 +1,21 @@
 class ScoreBoard
   def self.display_final_board(teams)
-    "#{self.display_headers}#{self.display_teams_result(teams)}"
+    self.display_headers + self.display_teams_result(teams)
   end
 
   def self.display_headers
-    "Team                           | MP |  W |  D |  L |  P\n"
+    display_line("Team", "MP",  "W", "D", "L", "P")
   end
 
   def self.display_teams_result(teams)
-    result = ""
-    teams.each do |team|
-    result += "#{team.name.ljust(31, " ")}|  #{team.match_played} |  #{team.won} |  #{team.drawn} |  #{team.lost} |  #{team.points}\n"
-    end
-    result
+    teams
+      .map { |team| display_line(team.name, team.match_played, team.won,  team.drawn, team.lost, team.points) }
+      .join
+  end
+
+  private
+
+  def self.display_line(*args)
+    format("%-31s| %2s | %2s | %2s | %2s | %2s\n", *args)
   end
 end
