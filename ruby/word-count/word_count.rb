e79a9e76b4c3c3a1b438dc8c3b4ee5921cdb9289
Mathieu Longe <mathieu.longe@orange.fr>
2020-11-24 13:57:06 +1100

    test 1 and 2 passing

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -6,3 +6,12 @@ To get started with TDD, see the `README.md` file in your
 `ruby/word-count` directory.
 =end
 
+class Phrase
+  def initialize(phrase)
+    @phrase = phrase
+  end
+
+  def word_count
+    @phrase.split(' ').each_with_object(Hash.new(0)) { |word, acc| acc[word] += 1 }
+  end
+end
