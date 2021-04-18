2e87df084b6f9fc9ad882d8abc5ee12054e6a843
Mathieu Longe <mathieu.longe@orange.fr>
2021-04-18 14:58:26 +1000

    raindrops done

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -6,3 +6,21 @@ To get started with TDD, see the `README.md` file in your
 `ruby/raindrops` directory.
 =end
 
+class Raindrops
+  def self.convert(num)
+    output = ''
+    output += 'Pling' if divisible_by?(num, 3)
+
+    output += 'Plang' if divisible_by?(num, 5)
+
+    output += 'Plong' if divisible_by?(num, 7)
+
+    output += num.to_s if output == ''
+
+    output
+  end
+
+  def self.divisible_by?(num, divider)
+    (num % divider).zero?
+  end
+end
