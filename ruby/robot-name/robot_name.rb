897fd697e15a2f657fabbc704c569267a0974add
Mathieu Longe <mathieu.longe@orange.fr>
2021-04-18 16:46:38 +1000

    robot name done

--- ruby/robot-name/robot_name.rb
+++ ruby/robot-name/robot_name.rb
@@ -6,3 +6,16 @@ To get started with TDD, see the `README.md` file in your
 `ruby/robot-name` directory.
 =end
 
+class Robot
+  def self.forget
+    @@available_names = [*'AA000'..'ZZ999'].shuffle
+  end
+
+  def name
+    @name ||= @@available_names.pop
+  end
+
+  def reset
+    @name = nil
+  end
+end
