d1fceba2d9403ff0425aecbdd177eb4913d26c6f
Mathieu Longe <mathieu.longe@orange.fr>
2021-04-21 21:25:17 +1000

    done

--- ruby/isogram/isogram.rb
+++ ruby/isogram/isogram.rb
@@ -5,4 +5,30 @@ Write your code for the 'Isogram' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/isogram` directory.
 =end
+require 'byebug'
 
+class Isogram
+  class << self
+    def isogram?(string)
+      repetition_of_each_letter = number_of_repetitions_for_each_letter(string)
+
+      are_equal_to_1?(repetition_of_each_letter.values)
+    end
+
+    private
+
+    def number_of_repetitions_for_each_letter(string)
+      letters_downcased(string).each_with_object(Hash.new(0)) do |char, hash|
+        hash[char] += 1
+      end
+    end
+
+    def letters_downcased(string)
+      string.downcase.scan(/\w/)
+    end
+
+    def are_equal_to_1?(values)
+      values.all? { |num| num == 1 }
+    end
+  end
+end
