61927681382d4f3f0668bd9f27528f7c27310be8
Mathieu Longe <mathieu.longe@orange.fr>
2021-05-04 20:45:05 +1000

    finishing scrabble

--- ruby/scrabble-score/scrabble_score.rb
+++ ruby/scrabble-score/scrabble_score.rb
@@ -5,4 +5,20 @@ Write your code for the 'Scrabble Score' exercise in this file. Make the tests i
 To get started with TDD, see the `README.md` file in your
 `ruby/scrabble-score` directory.
 =end
+class Scrabble
+  VALUE_LETTERS = {
+    'A' => 1, 'E' => 1, 'I' => 1, 'O' => 1, 'U' => 1, 'L' => 1, 'N' => 1, 'R' => 1, 'S' => 1, 'T' => 1, 'D' => 2, 'G' => 2, 'B' => 3, 'C' => 3, 'M' => 3, 'P' => 3, 'F' => 4, 'H' => 4, 'V' => 4, 'W' => 4, 'Y' => 4, 'K' => 5, 'J' => 8, 'X' => 8, 'Q' => 10, 'Z' => 10
+  }.freeze
 
+  def initialize(word)
+    @letters = word.to_s.strip.upcase.chars
+  end
+
+  def score
+    @letters.sum { |letter| VALUE_LETTERS[letter] }
+  end
+
+  def self.score(word)
+    new(word).score
+  end
+end
