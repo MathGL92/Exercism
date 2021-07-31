9c90ebf1732454ce7dc4e5c3e41aca12c32fa279
Mathieu Longe <mathieu.longe@orange.fr>
2021-07-31 17:07:54 +1000

    clock refactored

--- ruby/clock/clock.rb
+++ ruby/clock/clock.rb
@@ -1,50 +1,53 @@
-=begin
-Write your code for the 'Clock' exercise in this file. Make the tests in
-`clock_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/clock` directory.
-=end
+# Write your code for the 'Clock' exercise in this file. Make the tests in
+# `clock_test.rb` pass.
+#
+# To get started with TDD, see the `README.md` file in your
+# `ruby/clock` directory.
 require 'date'
+require 'byebug'
 
 class Clock
   attr_reader :minute, :hour
 
-  def initialize(hour: nil, minute: nil)
-    @date = Date.today
-    @hour = 0
-    @minute = manage_rolls_over_minute(minute) if minute
-    manage_rolls_over_hour(hour)
+  def initialize(hour: 0, minute: 0)
+    total_minutes = total_minutes(hour, minute)
+    @minute = calculate_minute(total_minutes)
+    @hour = calculate_hour(total_minutes)
   end
 
   def to_s
-    Time.new(@date.year, @date.month, @date.day, @hour, @minute).strftime('%H:%M')
+    format('%<hour>02d:%<minute>02d', hour: @hour, minute: @minute)
   end
 
   def +(other)
-    minute = @minute + other.minute
-    hour = @hour + other.hour
-    Clock.new(hour: hour, minute: minute).to_s
+    add_or_substract(other, __callee__)
   end
 
   def -(other)
-    minute = @minute - other.minute
-    hour = @hour - other.hour
-    Clock.new(hour: hour, minute: minute).to_s
+    add_or_substract(other, __callee__)
   end
 
   def ==(other)
-    @minute == other.minute && @hour == other.hour
+    to_s == other.to_s
   end
 
   private
 
-  def manage_rolls_over_hour(hour)
-    @hour = (@hour + (hour || 0)) % 24
+  def add_or_substract(other, method)
+    minute = @minute.send(method, other.minute)
+    hour = @hour.send(method, other.hour)
+    Clock.new(hour: hour, minute: minute).to_s
   end
 
-  def manage_rolls_over_minute(minute)
-    @hour += minute / 60
-    minute % 60
+  def calculate_hour(total_minute)
+    total_minute / 60
+  end
+
+  def calculate_minute(total_minute)
+    total_minute % 60
+  end
+
+  def total_minutes(hour, minute)
+    (hour * 60 + minute) % (24 * 60)
   end
 end
