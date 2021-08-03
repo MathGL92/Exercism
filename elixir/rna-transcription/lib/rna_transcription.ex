f64bc217e3da411ce858ea97059dc312b2adca41
Mathieu Longe <mathieu.longe@orange.fr>
2021-08-04 08:33:23 +1000

    refactor elixir rna-transcription

--- elixir/rna-transcription/lib/rna_transcription.ex
+++ elixir/rna-transcription/lib/rna_transcription.ex
@@ -7,11 +7,10 @@ defmodule RnaTranscription do
   iex> RnaTranscription.to_rna('ACTG')
   'UGAC'
   """
-  @dna_map %{ 'G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U' }
+  @dna_map %{ ?G => ?C, ?C => ?G, ?T => ?A, ?A => ?U }
 
   @spec to_rna([char]) :: [char]
   def to_rna(dna) do
-    dna
-    |> Enum.reduce('', fn(dna_element, acc) -> acc ++ @dna_map[[dna_element]] end)
+    Enum.map(dna, fn(dna_element) -> @dna_map[dna_element] end)
   end
 end
