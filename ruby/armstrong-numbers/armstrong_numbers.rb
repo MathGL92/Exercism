f6312ea6df53ef0411cc42ef3e184fe27c3af319
Mathieu Longe <mathieu.longe@orange.fr>
2021-11-28 16:46:31 +1100

    Finish armstrong numbers

--- ruby/armstrong-numbers/armstrong_numbers.rb
+++ ruby/armstrong-numbers/armstrong_numbers.rb
@@ -1,30 +1,7 @@
-=begin
-Write your code for the 'Armstrong Numbers' exercise in this file. Make the tests in
-`armstrong_numbers_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/armstrong-numbers` directory.
-=end
-
-class ArmstrongNumbers
-  class << self
-    def include?(integer)
-      digits_array = digits_to_array(integer)
-      integer == sum_of_digits_powered_to_the_number_of_digits(digits_array)
-    end
-
-    private
-
-    def digits_to_array(integer)
-      integer.digits
-    end
-
-    def number_of_digits(digits_array)
-      digits_array.size
-    end
-
-    def sum_of_digits_powered_to_the_number_of_digits(digits_array)
-      digits_array.sum { |num| num**number_of_digits(digits_array) }
-    end
+module ArmstrongNumbers
+  def include?(integer)
+    integer == integer.digits.sum { |digit| digit**integer.digits.size }
   end
 end
+
+ArmstrongNumbers.extend ArmstrongNumbers
